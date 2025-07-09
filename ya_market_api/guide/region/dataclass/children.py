0c6244ba1ebec4d2aede5e901586bf3e1ad03a43
Kirill_Lekhov <Kirill.Lekhov@gmail.com>
2025-07-09 11:09:37 +0300

    Improve RegionChildrenRequest

--- ya_market_api/guide/region/dataclass/children.py
+++ ya_market_api/guide/region/dataclass/children.py
@@ -4,12 +4,35 @@ from typing import Optional
 
 from pydantic.main import BaseModel
 from pydantic.fields import Field
+from pydantic.functional_validators import field_validator
 
 
 class Request(BaseModel):
 	region_id: int
 	page: Optional[int] = None
 	page_size: Optional[int] = Field(default=None, serialization_alias="pageSize")
+
+	@field_validator("page", mode="after")
+	@classmethod
+	def page_is_valid(cls, value: Optional[int]) -> Optional[int]:
+		if value is None:
+			return None
+
+		if value < 1 or value > 10_000:
+			raise ValueError("The page cannot be less than 1 or greater than 10000")
+
+		return value
+
+	@field_validator("page_size", mode="after")
+	@classmethod
+	def page_size_is_valid(cls, value: Optional[int]) -> Optional[int]:
+		if value is None:
+			return None
+
+		if value < 1:
+			raise ValueError("The page_size cannot be less than 1")
+
+		return value
 
 
 class ResponsePager(BaseModel):
